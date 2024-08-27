574e81ca2c812073311274a345326b27a0d246e5
mostafa-elnady <mostafa.elnady49@gmail.com>
2024-08-27 21:35:03 +0300

    provide add note Cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -5,7 +5,7 @@ import 'package:notes_app/cubits/add_note_cubit/add_note_state.dart';
 import 'package:notes_app/models/note_model.dart';
 
 class AddNoteCubit extends Cubit<AddNotesState> {
-  AddNoteCubit(super.initialState);
+  AddNoteCubit() : super(AddNotesInitial());
 
   addNote(NoteModel note) async {
     emit(AddNotesLoading());
