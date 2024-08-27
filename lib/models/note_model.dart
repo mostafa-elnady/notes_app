e293312e8721f53f9dcfc76fdaae09cbc8045cc1
mostafa-elnady <mostafa.elnady49@gmail.com>
2024-08-27 20:26:52 +0300

    register adapter

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -3,7 +3,7 @@ import 'package:hive/hive.dart';
 part 'note_model.g.dart';
 
 @HiveType(typeId: 0)
-class Note {
+class NoteModel {
   @HiveField(0)
   final String title;
   @HiveField(1)
@@ -13,7 +13,7 @@ class Note {
   @HiveField(3)
   final int color;
 
-  Note({
+  NoteModel({
     required this.title,
     required this.subTitle,
     required this.date,
--- lib/models/note_model.g.dart
+++ lib/models/note_model.g.dart
@@ -6,17 +6,17 @@ part of 'note_model.dart';
 // TypeAdapterGenerator
 // **************************************************************************
 
-class NoteAdapter extends TypeAdapter<Note> {
+class NoteModelAdapter extends TypeAdapter<NoteModel> {
   @override
   final int typeId = 0;
 
   @override
-  Note read(BinaryReader reader) {
+  NoteModel read(BinaryReader reader) {
     final numOfFields = reader.readByte();
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return Note(
+    return NoteModel(
       title: fields[0] as String,
       subTitle: fields[1] as String,
       date: fields[2] as String,
@@ -25,7 +25,7 @@ class NoteAdapter extends TypeAdapter<Note> {
   }
 
   @override
-  void write(BinaryWriter writer, Note obj) {
+  void write(BinaryWriter writer, NoteModel obj) {
     writer
       ..writeByte(4)
       ..writeByte(0)
@@ -44,7 +44,7 @@ class NoteAdapter extends TypeAdapter<Note> {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is NoteAdapter &&
+      other is NoteModelAdapter &&
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
