ef2bff6f2790a3e6a4809054d50320ce55d56ff6
mostafa-elnady <mostafa.elnady49@gmail.com>
2024-08-27 17:25:16 +0300

    create note model

---
+++ lib/models/node_model.dart
@@ -0,0 +1,13 @@
+class Note {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+  Note({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
+}
